995dd86cccd6fbcd9f8c96cf72bb8b3caf7fe50a
Joshua T. Rowe <jrowe@jrowe.org>
2022-04-22 13:10:25 -0400

    refactor to hash of hashes
    middle of refactor
    
    Ticket: #1 Add a cheatsheet document

--- src/QuickReference/data.pl
+++ src/QuickReference/data.pl
@@ -7,7 +7,21 @@ use YAML qw( Dump );
 
 $YAML::CompressSeries = 0;
 
+
 my @data = (
+    {
+        category => [ 'Document Tools', 'AsciiDoc' ],
+        name => 'AsciiDoc Home Page',
+        url => 'https://asciidoc-py.github.io/',
+    },
+    {
+        category => [ 'Document Tools', 'AsciiDoc' ],
+        name => 'AsciiDoc User Guide',
+        url => 'https://asciidoc-py.github.io/userguide.html',
+    },
+);
+
+my @data1 = (
     {
         name => 'AsciiDoc',
         references => [
@@ -33,39 +47,63 @@ my @data = (
     },
 );
 
-my $stash = {};
+my $root = get_cat(undef, '');
+my $stash = { links => $root };
 my @category_names;
 my %categories;
 
-foreach my $cat ( @data ) {
+foreach my $link ( @data ) {
+    my $link_name = $link->{name};
+
+    my $cat = get_cat( @{ $link->{categories} } );
+
+#FIXME for dev
+    $cat = $root;
+    my $items = $cat->{items} //= [];
+
     my $cat_name = $cat->{name};
-    $cat->{level} = 1;
-    $cat->{header_mark} = '=' x ( $cat->{level} + 1);
 
-    push @category_names, $cat_name;
+#    push @category_names, $cat_name;
 
-    my @items;
-    foreach my $ref ( @{ $cat->{references} } ) {
-        if ( 'Perl' eq $cat_name ) {
-            push @items, {
-                name => sprintf('%s @ MetaCPAN', $ref),
-                url => sprintf('http://metacpan.org/pod/%s', $ref),
-            };
-        }
-        else {
-            push @items, $ref;
-        }
-    }
+    #my @items;
+#    if ( 'Perl' eq $cat_name ) {
+#        push @items, {
+#            name => sprintf('%s @ MetaCPAN', $link_name),
+#            url => sprintf('http://metacpan.org/pod/%s', $link_name),
+#        };
+#    }
+#    else {
+        push @{ $items }, $link;
+#    }
 
-    @items = sort { $a->{name} cmp $b->{name} } @items;
-    $cat->{references} = \@items;
-    $categories{$cat_name} = $cat;
+    #@items = sort { $a->{name} cmp $b->{name} } @items;
+    #$cat->{references} = \@items;
+    #$categories{$cat_name} = $cat;
 }
 
-$stash->{_category_names} = [ sort @category_names ];
-$stash->{_categories} = \%categories;
+#$stash->{_category_names} = [ sort @category_names ];
+#$stash->{_categories} = \%categories;
+
+$stash->{links} = $root;
 
 print Dump($stash) . "\n";
 
 print STDERR "stash:\n" . np($stash) . "\n";
+
+
+sub get_cat {
+    my $parent = shift;
+    my @name   = @_;
+
+    my $path = join '/', @name;
+
+    if ( ! exists($categories{$path}) ) {
+        $categories{$path} = {
+            name  => $name[-1],
+            level => ( $parent ? ( $parent->{level} + 1 ) : 0 ),
+        };
+    }
+
+    return $categories{$path};
+}
 
